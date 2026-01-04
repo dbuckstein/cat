911c7ffe27b452c6a306b4e1c0ec2c91f31c8973
Daniel S. Buckstein <dbuckstein@users.noreply.github.com>
2026-01-04 14:18:07 -0500

    Begin image utility implementation.

--- cat/include/cat/utility/cat_image.h
+++ cat/include/cat/utility/cat_image.h
@@ -28,6 +28,61 @@
 cat_interface_begin;
 
 
+//! \typedef cat_pixel_color_t
+//! \brief Encoded pixel color.
+typedef uint64_t cat_pixel_color_t;
+
+
+//! \struct cat_image_s
+//! \brief Image descriptor.
+typedef struct cat_image_s
+{
+    uint32_t           image_width;   //< Width of image in pixels.
+    uint32_t           image_height;  //< Height of image in pixels.
+    cat_pixel_color_t* p_image_pixels;//< Pixel data.
+} cat_image_t;
+
+
+//! \fn cat_image_create
+//! \brief Allocate image data, setup descriptor.
+//! \param p_image Pointer to unused image descriptor (all fields are zero).
+//! \param image_width Width of image in pixels in range [0,32768].
+//! \param image_Height Height of image in pixels in range [0,32768].
+//! \return True if image created successfully.
+cat_decl bool cat_image_create(cat_image_t* const p_image, uint32_t const image_width, uint32_t const image_height);
+
+//! \fn cat_image_destroy
+//! \brief Deallocate image data, reset descriptor.
+//! \param p_image Pointer to active image descriptor (all fields are non-zero).
+//! \return True if image destroyed successfully.
+cat_decl bool cat_image_destroy(cat_image_t* const p_image);
+
+//! \fn cat_image_save_netpbm
+//! \brief Save image(s) in convenient NetPBM format(s).
+//! \param p_image Pointer to active image descriptor (all fields are non-zero).
+//! \param directory Save directory c-string (not including file name).
+//! \param file_name Save file name c-string (not including file path).
+//! \param using_alpha Alpha flag, true if alpha channel should be saved.
+//! \return True if image saved successfully.
+cat_decl bool cat_image_save_netpbm(cat_image_t const* const p_image, cstr_t const directory, cstr_t const file_name, bool const using_alpha);
+
+//! \fn cat_image_pixel_color
+//! \brief Create encoded pixel color given individual channels.
+//! \param r Red channel (16-bit).
+//! \param g Green channel (16-bit).
+//! \param b Blue channel (16-bit).
+//! \param a Alpha channel (16-bit).
+cat_decl cat_pixel_color_t cat_image_pixel_color(uint16_t const r, uint16_t const g, uint16_t const b, uint16_t const a);
+
+//! \fn cat_image_set_pixel
+//! \brief Set color for pixel(s) in image.
+//! \param p_image Pointer to active image descriptor (all fields are non-zero).
+//! \param pos_x Horizontal position of pixel in image; zero is left edge. Pass negative value for all columns.
+//! \param pos_y Vertical position of pixel; zero is top edge. Pass negative value for all rows.
+//! \param pixel_color Encoded color of pixel.
+//! \return True if pixel set successfully.
+cat_decl bool cat_image_set_pixel(cat_image_t const* const p_image, int32_t const pos_x, int32_t const pos_y, cat_pixel_color_t const pixel_color);
+
 
 cat_interface_end;
 
--- cat/source/cat/utility/cat_image.c
+++ cat/source/cat/utility/cat_image.c
@@ -26,6 +26,91 @@
 cat_implementation_begin;
 
 
+static inline size_t cat_image_internal_index(size_t const image_width, size_t const pos_x, size_t const pos_y)
+{
+    return (image_width * pos_y + pos_x);
+}
+
+
+cat_impl bool cat_image_create(cat_image_t* const p_image, uint32_t const image_width, uint32_t const image_height)
+{
+    assert_or_bail(p_image) false;
+    assert_or_bail(p_image->image_width == 0) false;
+    assert_or_bail(p_image->image_height == 0) false;
+    assert_or_bail(p_image->p_image_pixels == NULL) false;
+    assert_or_bail(image_width > 0) false;
+    assert_or_bail(image_height > 0) false;
+    assert_or_bail(image_width <= 32768) false;
+    assert_or_bail(image_height <= 32768) false;
+
+    return true;
+}
+
+cat_impl bool cat_image_destroy(cat_image_t* const p_image)
+{
+    assert_or_bail(p_image) false;
+    assert_or_bail(p_image->image_width != 0) false;
+    assert_or_bail(p_image->image_height != 0) false;
+    assert_or_bail(p_image->p_image_pixels != NULL) false;
+
+    return true;
+}
+
+cat_impl bool cat_image_save_netpbm(cat_image_t const* const p_image, cstr_t const directory, cstr_t const file_name, bool const using_alpha)
+{
+    assert_or_bail(p_image) false;
+    assert_or_bail(p_image->image_width != 0) false;
+    assert_or_bail(p_image->image_height != 0) false;
+    assert_or_bail(p_image->p_image_pixels != NULL) false;
+    assert_or_bail(directory != NULL) false;
+    assert_or_bail(file_name != NULL) false;
+
+    if (using_alpha)
+    {
+    }
+    return true;
+}
+
+cat_impl cat_pixel_color_t cat_image_pixel_color(uint16_t const r, uint16_t const g, uint16_t const b, uint16_t const a)
+{
+    cat_pixel_color_t pixel_color = a;
+    pixel_color |= (cat_pixel_color_t)b << 16;
+    pixel_color |= (cat_pixel_color_t)g << 32;
+    pixel_color |= (cat_pixel_color_t)r << 48;
+    return pixel_color;
+}
+
+cat_impl bool cat_image_set_pixel(cat_image_t const* const p_image, int32_t const pos_x, int32_t const pos_y, cat_pixel_color_t const pixel_color)
+{
+    bool const all_x = (pos_x < 0);
+    bool const all_y = (pos_y < 0);
+    assert_or_bail(p_image) false;
+    assert_or_bail(p_image->image_width != 0) false;
+    assert_or_bail(p_image->image_height != 0) false;
+    assert_or_bail(p_image->p_image_pixels != NULL) false;
+
+    if (all_x && all_y)
+    {
+        cat_pixel_color_t const* const p_pixel_end = p_image->p_image_pixels + (size_t)p_image->image_width * (size_t)p_image->image_height;
+        cat_pixel_color_t* p_pixel = p_image->p_image_pixels;
+        while (p_pixel < p_pixel_end)
+        {
+            *(p_pixel++) = pixel_color;
+        }
+    }
+    else if (all_x)
+    {
+    }
+    else if (all_y)
+    {
+    }
+    else
+    {
+    }
+    return true;
+}
+
+
 #include "cat/utility/cat_time.h"
 #include "cat/utility/cat_console.h"
 
